509789a8cbc0f2776ad49ecf7fce9a129d9c4beb
shivamgupta1830 <shivamgupta1830@gmail.com>
2024-04-04 10:57:03 +0530

    functionality Done

--- controllers/products.js
+++ controllers/products.js
@@ -1,7 +1,7 @@
 const Product = require("../models/products");
 
 const getAllProducts = async (req, res) => {
-  const { featured, company, name } = req.query;
+  const { featured, company, name, sort, fields } = req.query;
   const queryObject = {};
 
   if (featured) {
@@ -12,30 +12,46 @@ const getAllProducts = async (req, res) => {
   if (company) {
     queryObject.company = company;
   }
+
   if (name) {
     queryObject.name = { $regex: name, $options: "i" };
   }
-  console.log(queryObject);
-  const products = await Product.find(queryObject);
+
+  let result = Product.find(queryObject);
+
+  //Sort
+
+  if (sort) {
+    const sortList = sort.split(",").join(" ");
+    result = result.sort(sortList);
+  } else {
+    result = result.sort("createdAt");
+  }
+
+  //fields
+
+  if (fields) {
+    const fieldsList = fields.split(",").join(" ");
+    result = result.select(fieldsList);
+  }
+
+  //pagination
+  const page = Number(req.query.page);
+  const limit = Number(req.query.limit);
+
+  const skip = (page - 1) * limit;
+
+  result = result.skip(skip).limit(limit);
+
+  //final result
+
+  const products = await result;
+
   res
     .status(200)
     .json({ msg: "success", total_results: products.length, products });
 };
 
-const getSingleProduct = async (req, res) => {
-  res.status(200).json({ msg: "success", data: "Single product" });
-};
-
-const updateProduct = async (req, res) => {
-  res.status(200).json({ msg: "success", data: "update product" });
-};
-const deleteProduct = async (req, res) => {
-  res.status(200).json({ msg: "success", data: "delete product" });
-};
-
 module.exports = {
   getAllProducts,
-  getSingleProduct,
-  updateProduct,
-  deleteProduct,
 };
--- routes/products.js
+++ routes/products.js
@@ -1,18 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
-const {
-  getAllProducts,
-  getSingleProduct,
-  updateProduct,
-  deleteProduct,
-} = require("../controllers/products");
+const { getAllProducts } = require("../controllers/products");
 
 router.route("/").get(getAllProducts);
-router
-  .route("/:id")
-  .get(getSingleProduct)
-  .patch(updateProduct)
-  .delete(deleteProduct);
 
 module.exports = router;
